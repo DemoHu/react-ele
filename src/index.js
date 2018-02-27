81aaa2696655eaca404247e0d6169bb673dce02e
zhengxing-cn <liuzhenixng@cianiaomeishi.cn>
2018-02-28 00:59:57 +0800

    搭建App框架

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App/App';
-import AddressDetail from './pages/AddressDetail/AddressDetail'
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { createStore,applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import {rootReducer} from './reducers/index'
 import thunkMiddleware from 'redux-thunk'
+import App from './pages/App/App';
+import AddressDetail from './pages/AddressDetail/AddressDetail'
+import Home from './pages/Home/Home'
+import Search from './pages/Search/Search'
+import Order from './pages/Order/Order'
+import My from './pages/My/My'
+
+
 let store = createStore(rootReducer,applyMiddleware(thunkMiddleware))
 ReactDOM.render(
   <Provider store={store}>
-    <Router >
+    <Router  >
       <div>
         <Route exact path="/" component={App} />
-        <Route exact path="/city/:id/:name" component={AddressDetail} />
+        <Route  path="/city/:id/:name" component={AddressDetail} />
+        <Route  path="/home" component={Home} />
+        <Route  path="/search" component={Search} />
+        <Route  path="/order" component={Order} />
+        <Route  path="/my" component={My} />
       </div>
     </Router>
   </Provider>
